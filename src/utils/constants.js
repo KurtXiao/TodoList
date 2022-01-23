f2aac8285b7419592c5d8c951dac069b99b03591
KurtXiao <zechuan.xiao@vanderbilt.edu>
2022-01-23 02:10:57 -0600

    first commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const HOST_NAME = '18.234.65.90:8080';
+export const HOST_NAME = 'http://3.227.214.87:8080';
 export const INVALID_USERNAME = "INVALID_USERNAME";
 export const WRONG_PASSWORD = "WRONG_PASSWORD";
 export const SINGLE_DATE_CALENDAR = "SINGLE_DATE_CALENDAR";
